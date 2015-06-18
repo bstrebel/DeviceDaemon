b849dd8b8be9820d990df24de540c30199ab8ffe
Bernd Strebel (E6530) <bernd.strebel@googlemail.com>
2015-06-18 05:17:03 +0200

    class HttpDevice adatped to new Device model

--- DeviceLib/HttpDevice.py
+++ DeviceLib/HttpDevice.py
@@ -24,15 +24,21 @@ from Device import *
 
 
 class HttpDevice(Device):
-    def __init__(self, logger, serial, name):
+
+    def __init__(self, logger, device):
+
+        assert isinstance(logger, logging.Logger)
         self._logger = logger
-        Device.__init__(self, logger=self._logger, address=serial, name=name)
+        Device.__init__(self, self._logger, device)
 
     def check(self, callback):
         return True
 
     @property
-    def serial(self): return self._address
+    def serial(self): return Device.config(self,'serial')
+
+    @property
+    def name(self): return Device.config(self,'display')
 
 
 class HttpRequestHandler(BaseHTTPRequestHandler):
@@ -46,14 +52,14 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         # OPTS= self.server.options
         GET = parse_qs(urlparse(self.path).query)
         if GET:
-            (response,message) = self.server.discover.process_get_request(GET, self)
+            (response, message) = self.server.discover.process_get_request(GET, self)
             self.send_response(response)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(message)
             self.wfile.write('\n')
-            self.wfile.write(threading.current_thread().getName())
-            self.wfile.write('\n')
+            # self.wfile.write(threading.current_thread().getName())
+            # self.wfile.write('\n')
 
 class HttpServer(ThreadingMixIn, HTTPServer):
     """
@@ -78,9 +84,11 @@ class HttpDiscoverDevice:
     #     return {'0': 0, 'exit': 0, 'leave': 0, 'out': '0',
     #             '1': 1, 'enter': 1, 'in': 1}[key]
 
-    def __init__(self, options):
+    def __init__(self, logger, options, devices):
 
-        self._logger = options['logger']
+        assert isinstance(logger, logging.Logger)
+        self._logger = logger
+
         self._callback = options['callback']
         self._port = int(options['port'])
         self._host = options['host']
@@ -88,11 +96,23 @@ class HttpDiscoverDevice:
         self._socket = None
         self._devices = {}
 
-        for key in options['devices']:
-            self._devices[key] = HttpDevice(self._logger, key, options['devices'][key])
+        for device in options['devices']:
+            if device in devices:
+                devices[device]['key'] = device
+                if 'callback' not in devices[device]:
+                    devices[device]['callback'] = options['callback']
+                key = options['key']
+                if key in devices[device]:
+                    id = devices[device][key]
+                    self._devices[id] = HttpDevice(self._logger, devices[device])
+                    self._logger.info("Device [%s] with %s [%s] registered for http requests!" %
+                                     (self._devices[id].key, key, self._devices[id].config(key)))
 
     def process_get_request(self, request, handler):
+
         assert isinstance(handler, HttpRequestHandler)
+
+        # process zone update request for device
         if 'device' in request and len(request['device']) == 1:
             key = request['device'][0]
             if key in self._devices:
@@ -103,8 +123,9 @@ class HttpDiscoverDevice:
                         if update in self._update:
                             self._devices[key].zone = zone
                             self._devices[key].update = self._update[update]
-                            return self._callback['update'](self._devices[key])
+                            return Device.callback(self._devices[key], 'update')
 
+        # process other get requests
         return self._callback['request'](request, handler)
 
     def __enter__(self):
@@ -129,7 +150,6 @@ class HttpDiscoverDevice:
         #     if self._httpd.socket in rfds:
         #         self._httpd.handle_request()
 
-
     def close(self):
         if self._httpd is not None:
             self._httpd.server_close()
@@ -150,12 +170,8 @@ if __name__ == '__main__':
 
     def http_zone_changed(http_device):
         assert isinstance(http_device, HttpDevice)
-        #print("Device %s with serial %s %s zone %s" % http_device.name,
-        #      http_device.serial, "entered" if http_device.update else "left", http_device.zone)
-
-        message = "Device %s with serial %s %s zone %s" %\
-                  (http_device.name, http_device.serial,"entered" if http_device.update else "left", http_device.zone)
-
+        message = "Device %s with serial %s %s zone %s" % \
+                  (http_device.name, http_device.serial, "entered" if http_device.update else "left", http_device.zone)
         return 200, message
 
     def request(request, handler):
@@ -165,10 +181,17 @@ if __name__ == '__main__':
     options = {'logger': logging.getLogger(),
                'host': '0.0.0.0',
                'port': 8080,
-               'devices': {'R32D102JR6N': "Nexus 10", 'SH42NW901328': "HTC One"},
-               'callback': {'update': http_zone_changed, 'request': request}}
+               'key': 'serial',
+               'request': request,
+               'devices': ['nexus', 'htc'],
+               'callback': {'update': http_zone_changed}}
 
-    with HttpDiscoverDevice(options) as discover:
+    devices = {'nexus': {'serial': 'R32D102JR6N',  'display': "Google Nexus 10"},
+               'htc':   {'serial': 'SH42NW901328', 'display': "HTC One M7"} }
+
+    logger = logging.getLogger()
+
+    with HttpDiscoverDevice(logger, options, devices) as discover:
 
         try:
 
